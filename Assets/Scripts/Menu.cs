32afb52b7f00a756cd4eb3f277341f2a32a84a5f
Asbibi <66073617+Asbibi@users.noreply.github.com>
2021-12-16 01:39:30 +0100

    Final commit : quick balance + added end game text to know if won or not

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -2,9 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
+    public Text wintext;
+
+    private void Start()
+    {
+        if (wintext != null)
+            wintext.text = PlayerPrefs.GetInt("Won") == 1 ? "You won the Challenge !" : "You FAILED...";
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene(1);
